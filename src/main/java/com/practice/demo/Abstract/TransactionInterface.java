60f5d03e465eff627f9922383a58178a34293997
Paul <rladudwns804@gmail.com>
2022-05-11 13:58:06 -0500

    created implementation

--- src/main/java/com/practice/demo/Abstract/TransactionInterface.java
+++ src/main/java/com/practice/demo/Abstract/TransactionInterface.java
@@ -1,4 +1,20 @@
 package com.practice.demo.Abstract;
 
+import java.util.Date;
+
 public interface TransactionInterface {
+
+
+    public long getTransactionID();
+    public void setTransactionID(long transactionID);
+    public long getAccountNum();
+    public void setAccountNum(long accountNum);
+    public TransactionType getType();
+    public void setType(TransactionType type);
+    public double getTransactionAmt();
+    public void setTransactionAmt(double transactionAmt);
+    public Date getTransactionDate();
+    public void setTransactionDate(Date transactionDate);
+
 }
+

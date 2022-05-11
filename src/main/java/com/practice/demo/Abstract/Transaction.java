708a8957da5eb1bd480052085333ccad16608b8d
Paul <rladudwns804@gmail.com>
2022-05-11 13:56:33 -0500

    created implementation for Transaction abstract class

--- src/main/java/com/practice/demo/Abstract/Transaction.java
+++ src/main/java/com/practice/demo/Abstract/Transaction.java
@@ -1,8 +1,25 @@
 package com.practice.demo.Abstract;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.MappedSuperclass;
+import java.util.Date;
 
 @MappedSuperclass
-public abstract Transaction {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class Transaction {
+
+
+    private long transactionID;
+    private long accountNum;
+    private TransactionType type;
+    private double transactionAmt;
+    private Date transactionDate;
+
+
 
 }
